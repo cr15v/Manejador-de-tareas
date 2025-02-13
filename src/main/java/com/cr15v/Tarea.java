f0a65978be0426cfb06118ad77a5148f2de64321
cr15v <144637940+cr15v@users.noreply.github.com>
2025-02-13 15:10:52 -0600

    Create Tarea.java

---
+++ src/main/java/com/cr15v/Tarea.java
@@ -0,0 +1,23 @@
+package com.cr15v;
+
+public class Tarea {
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void completar() {
+        this.completada = true;
+    }
+}
